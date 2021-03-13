62ccc6fa8634105344a0aded7893ab347bb78651
Triwandha Febian Rizqi Ramadhan <triwandha@warungpintar.co>
2021-03-14 00:24:56 +0700

    add test for get error database

--- internal/domain/currencies/errors/errors_test.go
+++ internal/domain/currencies/errors/errors_test.go
@@ -39,3 +39,22 @@ func TestGetErrorInvalidPayload(t *testing.T) {
 		})
 	}
 }
+
+func TestGetErrorDatabase(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "error message",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := GetErrorDatabase(); (err != nil) != tt.wantErr {
+				t.Errorf("GetErrorDatabase() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
