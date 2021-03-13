1b2561ad7843610e6675a6ab12ac5fcbb4a846ed
Triwandha Febian Rizqi Ramadhan <triwandha@warungpintar.co>
2021-03-13 18:43:21 +0700

    add error and test for currency domain

--- internal/domain/currencies/errors/errors.go
+++ internal/domain/currencies/errors/errors.go
@@ -6,14 +6,18 @@ func GetErrorInvalidPayload() error {
 	return errors.New("error invalid payload")
 }
 
-func GetErrorConversionAlreadyExist() error {
-	return errors.New("error this conversion already exist")
-}
-
-func GetErrorConversionNotFound() error {
-	return errors.New("error no conversion for these currency")
-}
+//
+//func GetErrorConversionAlreadyExist() error {
+//	return errors.New("error this conversion already exist")
+//}
+//
+//func GetErrorConversionNotFound() error {
+//	return errors.New("error no conversion for these currency")
+//}
 
 func GetErrorCurrencyAlreadyExist() error {
 	return errors.New("error currency already exist")
+}
+func GetErrorDatabase() error {
+	return errors.New("database error")
 }
--- internal/domain/currencies/errors/errors_test.go
+++ internal/domain/currencies/errors/errors_test.go
@@ -1,1 +1,41 @@
 package errors
+
+import "testing"
+
+func TestGetErrorCurrencyAlreadyExist(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "error happens",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := GetErrorCurrencyAlreadyExist(); (err != nil) != tt.wantErr {
+				t.Errorf("GetErrorCurrencyAlreadyExist() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestGetErrorInvalidPayload(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "error happens",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := GetErrorInvalidPayload(); (err != nil) != tt.wantErr {
+				t.Errorf("GetErrorInvalidPayload() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
