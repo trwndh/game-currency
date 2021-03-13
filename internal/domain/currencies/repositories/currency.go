8e3ca3587260b9817e25aa5e6629e4389484c948
Triwandha Febian Rizqi Ramadhan <triwandha@warungpintar.co>
2021-03-13 18:45:12 +0700

    add currency repository interface

--- internal/domain/currencies/repositories/currency.go
+++ internal/domain/currencies/repositories/currency.go
@@ -1,1 +1,13 @@
 package repositories
+
+import (
+	"context"
+
+	"github.com/trwndh/game-currency/internal/domain/currencies/entity"
+)
+
+type Currency interface {
+	CountByName(ctx context.Context, name string) (int32, error)
+	Create(ctx context.Context, params entity.CurrencyDAO) error
+	Find(ctx context.Context) ([]entity.CurrencyDAO, error)
+}
