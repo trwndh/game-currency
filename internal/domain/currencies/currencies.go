4ffbf9df8cdbfbbe064a8a452199455c9f7a81dd
Triwandha Febian Rizqi Ramadhan <triwandha@warungpintar.co>
2021-03-13 18:42:21 +0700

    create interface for currency

--- internal/domain/currencies/currencies.go
+++ internal/domain/currencies/currencies.go
@@ -1,1 +1,12 @@
 package currencies
+
+import (
+	"context"
+
+	"github.com/trwndh/game-currency/internal/domain/currencies/dto"
+)
+
+type Service interface {
+	Find(ctx context.Context) (dto.GetCurrenciesResponse, error)
+	Create(ctx context.Context, param dto.CreateCurrencyRequest) (dto.CreateCurrencyResponse, error)
+}
