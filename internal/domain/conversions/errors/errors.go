375fd591a12167d44d321aeb4d13e320106c2d44
Triwandha Febian Rizqi Ramadhan <triwandha@warungpintar.co>
2021-03-13 19:35:05 +0700

    add errors in conversion

--- internal/domain/conversions/errors/errors.go
+++ internal/domain/conversions/errors/errors.go
@@ -6,18 +6,14 @@ func GetErrorInvalidPayload() error {
 	return errors.New("error invalid payload")
 }
 
-//
-//func GetErrorConversionAlreadyExist() error {
-//	return errors.New("error this conversion already exist")
-//}
-//
-//func GetErrorConversionNotFound() error {
-//	return errors.New("error no conversion for these currency")
-//}
-
-func GetErrorCurrencyAlreadyExist() error {
-	return errors.New("error currency already exist")
+func GetErrorConversionAlreadyExist() error {
+	return errors.New("error this conversion already exist")
 }
+
+func GetErrorConversionNotFound() error {
+	return errors.New("error no conversion for these currency")
+}
+
 func GetErrorDatabase() error {
 	return errors.New("database error")
 }
--- internal/domain/conversions/errors/errors_test.go
+++ internal/domain/conversions/errors/errors_test.go
@@ -1,1 +1,79 @@
 package errors
+
+import "testing"
+
+func TestGetErrorConversionAlreadyExist(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "error message",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := GetErrorConversionAlreadyExist(); (err != nil) != tt.wantErr {
+				t.Errorf("GetErrorConversionAlreadyExist() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestGetErrorConversionNotFound(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "error message",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := GetErrorConversionNotFound(); (err != nil) != tt.wantErr {
+				t.Errorf("GetErrorConversionNotFound() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestGetErrorInvalidPayload(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "error message",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := GetErrorInvalidPayload(); (err != nil) != tt.wantErr {
+				t.Errorf("GetErrorInvalidPayload() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestGetErrorDatabase(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "error message",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := GetErrorDatabase(); (err != nil) != tt.wantErr {
+				t.Errorf("GetErrorDatabase() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
