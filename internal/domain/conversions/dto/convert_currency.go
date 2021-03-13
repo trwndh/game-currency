0f7a37c69ddfb518c8191e8ac0579cba2489c816
Triwandha Febian Rizqi Ramadhan <triwandha@warungpintar.co>
2021-03-13 19:33:40 +0700

    add conversion dto

--- internal/domain/conversions/dto/convert_currency.go
+++ internal/domain/conversions/dto/convert_currency.go
@@ -1,1 +1,12 @@
 package dto
+
+type ConvertCurrencyRequest struct {
+	CurrencyIDFrom int64 `json:"currency_id_from"`
+	CurrencyIDTo   int64 `json:"currency_id_to"`
+	Amount         int64 `json:"amount"`
+}
+
+type ConvertCurrencyResponse struct {
+	Status string `json:"status"`
+	Error  string `json:"error,omitempty"`
+}
--- internal/domain/conversions/dto/create_conversion.go
+++ internal/domain/conversions/dto/create_conversion.go
@@ -1,1 +1,20 @@
 package dto
+
+type CreateConversionRequest struct {
+	CurrencyIDFrom int64 `json:"currency_id_from"`
+	CurrencyIDTo   int64 `json:"currency_id_to"`
+	Rate           int64 `json:"rate"`
+}
+
+func (c CreateConversionRequest) IsCurrencyIDFromEmpty() bool {
+	return c.CurrencyIDFrom == 0
+}
+
+func (c CreateConversionRequest) IsCurrencyIDToEmpty() bool {
+	return c.CurrencyIDTo == 0
+}
+
+type CreateConversionResponse struct {
+	Status string `json:"status"`
+	Error  string `json:"error,omitempty"`
+}
