e283877b4982774f49166dd7d6ae9c49d8527c8d
Triwandha Febian Rizqi Ramadhan <triwandha@warungpintar.co>
2021-03-13 18:42:56 +0700

    add entity for currency

--- internal/domain/currencies/entity/currency.go
+++ internal/domain/currencies/entity/currency.go
@@ -1,1 +1,8 @@
 package entity
+
+type CurrencyDAO struct {
+	ID   int64  `json:"id" db:"id"`
+	Name string `json:"name" db:"name"`
+	//CreatedAt time.Time `json:"created_at"`
+	//UpdatedAt time.Time `json:"updated_at"`
+}
