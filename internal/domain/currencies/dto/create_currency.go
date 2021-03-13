55e7b173c1738f48b5e1694db1fa16962f5af0c4
Triwandha Febian Rizqi Ramadhan <triwandha@warungpintar.co>
2021-03-13 18:41:36 +0700

    add DTO for currency service

--- internal/domain/currencies/dto/create_currency.go
+++ internal/domain/currencies/dto/create_currency.go
@@ -1,5 +1,14 @@
-package entity
+package dto
 
-type CreateCurrencyDAO struct {
+type CreateCurrencyRequest struct {
 	Name string `json:"name"`
+}
+
+func (c CreateCurrencyRequest) IsNameEmpty() bool {
+	return c.Name == ""
+}
+
+type CreateCurrencyResponse struct {
+	Status string `json:"status,omitempty"`
+	Error  string `json:"error,omitempty"`
 }
--- internal/domain/currencies/dto/get_currencies.go
+++ internal/domain/currencies/dto/get_currencies.go
@@ -1,1 +1,13 @@
 package dto
+
+type Currency struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
+}
+
+type GetCurrenciesRequest struct{}
+
+type GetCurrenciesResponse struct {
+	Currencies []Currency `json:"currencies"`
+	Error      string     `json:"error,omitempty"`
+}
