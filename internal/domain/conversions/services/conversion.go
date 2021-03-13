7b2e40411b42cfab8b912c82404c9071b737a2d0
Triwandha Febian Rizqi Ramadhan <triwandha@warungpintar.co>
2021-03-13 19:35:38 +0700

    add conversion services

--- internal/domain/conversions/services/conversion.go
+++ internal/domain/conversions/services/conversion.go
@@ -1,1 +1,14 @@
 package services
+
+import (
+	"github.com/trwndh/game-currency/internal/domain/conversions"
+	"github.com/trwndh/game-currency/internal/domain/conversions/repositories"
+)
+
+type service struct {
+	ConversionRepo repositories.Conversion
+}
+
+func NewConversion(ConversionRepo repositories.Conversion) conversions.Service {
+	return service{ConversionRepo: ConversionRepo}
+}
--- internal/domain/conversions/services/create_conversion.go
+++ internal/domain/conversions/services/create_conversion.go
@@ -1,1 +1,41 @@
 package services
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+
+	"github.com/trwndh/game-currency/internal/instrumentation/loggers"
+
+	"github.com/trwndh/game-currency/internal/domain/conversions/errors"
+
+	"github.com/opentracing/opentracing-go"
+
+	"github.com/trwndh/game-currency/internal/domain/conversions/dto"
+)
+
+func (s service) Create(ctx context.Context, param dto.CreateConversionRequest) (dto.CreateConversionResponse, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "[Conversion][Service][Create]")
+	defer span.Finish()
+
+	if param.IsCurrencyIDFromEmpty() || param.IsCurrencyIDToEmpty() {
+		return dto.CreateConversionResponse{Error: errors.GetErrorInvalidPayload().Error()}, errors.GetErrorInvalidPayload()
+	}
+
+	count, err := s.ConversionRepo.IsAlreadyExist(ctx, param)
+	if err != nil {
+		loggers.For(ctx).Error(errors.GetErrorDatabase().Error(), zap.Error(err))
+	}
+
+	if count > 0 {
+		return dto.CreateConversionResponse{Error: errors.GetErrorConversionAlreadyExist().Error()}, errors.GetErrorConversionAlreadyExist()
+	}
+
+	err = s.ConversionRepo.Create(ctx, param)
+	if err != nil {
+		loggers.For(ctx).Error(errors.GetErrorDatabase().Error(), zap.Error(err))
+		return dto.CreateConversionResponse{Error: errors.GetErrorDatabase().Error()}, errors.GetErrorDatabase()
+	}
+
+	return dto.CreateConversionResponse{Status: "success"}, nil
+}
